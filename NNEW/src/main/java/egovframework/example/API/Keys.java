ccfdb3487104d39dfc08980f9b38165f011f7289
highlyko17 <76466882+highlyko17@users.noreply.github.com>
2023-08-29 18:28:31 +0900

    Update Keys.java

--- NNEW/src/main/java/egovframework/example/API/Keys.java
+++ NNEW/src/main/java/egovframework/example/API/Keys.java
@@ -1,9 +1,9 @@
 package egovframework.example.API;
 
 public class Keys {
-	public static String OPENAPI_KEY = "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";
-	public static String OPENAPI_ORGKEY = "org-0cFVaADh2SqSCqr8bgPKTJMv";
-	public static String PINECONE_KEY = "106018db-f9fb-4675-8bb1-3c3cfcecfc7c";
+	public static String OPENAPI_KEY = "";
+	public static String OPENAPI_ORGKEY = "";
+	public static String PINECONE_KEY = "";
 	
 	public static Keys keys = new Keys();
 	public Keys getInstance() {
